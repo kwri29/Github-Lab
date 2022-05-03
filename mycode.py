956dc3f757913b71e3dae9ab731c82f3235569d6
Khloe Wright <khloe.wright@bison.howard.edu>
2022-05-03 02:12:51 +0000

    final code that prints output in terminal

--- mycode.py
+++ mycode.py
@@ -1,37 +1,39 @@
 import sys
 
+shift_num = int(sys.argv[1])
+uppercase_message = ""
 for line in sys.stdin:
-    line = line.upper()
+    uppercase_message += line.upper()
 
-def mycipher(shift_num, txt):
   
-  block = 0 #keeps track of 5 letters per block
-  per_line = 0 #keepts track of 10 blocks per line
-  encrypted = ""
+block = 0 #keeps track of 5 letters per block
+per_line = 0 #keepts track of 10 blocks per line
+encrypted = ""
 
-  for char in txt:
-    asc = ord(char) #to find the value in ascii of the character so we can encrypt
-    if (asc >= 65 and asc <= 90):
-      is_letter = True #do this to make sure we aren't changing the character of any spaces/specia    l characters
-    else:
-      is_letter = False
-    if not(is_letter):
-      continue
-    else:
-      block += 1
-      new_char = chr((asc + shift_num-65) % 26 + 65)
-      if block == 5:
-        per_line += 1
-        if per_line == 10:
-          encrypted = encrypted + new_char + "\n"
-          per_line = 0 #resets the var
-          block = 0
-        else:
-          encrypted = encrypted + new_char + " "
-          block = 0 #resets the var
+
+for char in uppercase_message:
+  asc = ord(char) #to find the value in ascii of the character so we can encrypt
+  if (asc >= 65 and asc <= 90):
+    is_letter = True #do this to make sure we aren't changing the character of any spaces/specia    l characters
+  else:
+    is_letter = False
+  if not(is_letter):
+    continue
+  else:
+    block += 1
+    new_char = chr((asc + shift_num-65) % 26 + 65)
+    if block == 5:
+      per_line += 1
+      if per_line == 10:
+        encrypted = encrypted + new_char + "\n"
+        per_line = 0 #resets the var
+        block = 0
       else:
-        encrypted += new_char
+        encrypted = encrypted + new_char + " "
+        block = 0 #resets the var
+    else:
+      encrypted += new_char
 
-  print(encrypted)
+print(encrypted)
      
 
